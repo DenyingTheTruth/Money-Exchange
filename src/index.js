e10698d4371d817d1f8824a748a14d0f59b44306
DenyingTheTruth <anton.bachikin@gmail.com>
2018-09-22 20:29:06 +0300

    VERY MUCH 'IF'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var coins ={};
+    var buf = 0;
+    if(currency >= 10000){
+      coins.error = "You are rich, my friend! We don't have so much coins for exchange";
+    }
+    else{
+      buf = Math.floor(currency/50);
+      currency = currency - buf*50;
+      if (buf > 0) coins.H = buf;
+      if (currency !== 0){
+        buf = Math.floor(currency/25);
+        currency = currency - buf*25;
+        if (buf > 0) coins.Q = buf;
+        if (currency !== 0){
+          buf = Math.floor(currency/10);
+          currency = currency - buf*10;
+          if (buf > 0) coins.D = buf;
+          if (currency !== 0){
+            buf = Math.floor(currency/5);
+            currency = currency - buf*5;
+            if (buf > 0) coins.N = buf;
+            if (currency !== 0){
+              buf = 0;
+              while (currency !== 0){
+                currency--;
+                buf ++;
+              }
+              if (buf > 0) coins.P = buf;
+            }
+          }
+        }
+      }
+    }
+  
+    return coins;
 }
